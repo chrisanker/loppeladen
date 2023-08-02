831e6175ea4b3c6d13aec7a711ce1b7e00a33bcd
Christian Anker Larsen <calanker@gmail.com>
2023-08-02 20:24:30 +0200

    added open and close menu feature

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("overlay-menu").style.width = "30em";
+}
+
+function closeNav() {
+    document.getElementById("overlay-menu").style.width = "0";
+}
