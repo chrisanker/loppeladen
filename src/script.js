39f07767810c364e5ab6e8121dcc46f9438f7bec
Christian Anker Larsen <calanker@gmail.com>
2023-09-28 23:20:26 +0200

    implemented dynamic list of opening dates based on current date

--- src/script.js
+++ src/script.js
@@ -1,7 +1,85 @@
+window.onload = (event) => {
+    renderOpeningDates();
+}
+
 function openNav() {
     document.getElementById("overlay-menu").style.width = "30em";
 }
 
 function closeNav() {
     document.getElementById("overlay-menu").style.width = "0";
+}
+
+const openingTimes = [
+    {
+        date: new Date("15 Aug 2023")
+    },
+    {
+        date: new Date("02 October 2023")
+    },
+    {
+        date: new Date("07 November 2023")
+    },
+    {
+        date: new Date("06 December 2023")
+    }
+];
+
+function renderOpeningDates() {
+    const today = new Date();
+    const openingDatesList = document.querySelector("#opening-dates-list");
+    openingTimes.forEach((item) => {
+        if (today <= item.date) {
+            const formattedDate = formatDate(item.date);
+            const dayOfWeek = getDayOfWeek(item.date.getDay());
+            const listItem = document.createElement("li");
+            const listItemNode = document.createTextNode(dayOfWeek + " " + formattedDate);
+            listItem.appendChild(listItemNode);
+            openingDatesList.appendChild(listItem);
+        }
+    });
+    
+}
+
+function formatDate(date) {
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const formattedMonth = getMonthName(month)
+
+    return `${day}. ${formattedMonth} ${year}`
+}
+
+function getDayOfWeek(date) {
+    const days = [
+        "Søndag",
+        "Mandag",
+        "Tirsdag",
+        "Onsdag",
+        "Torsdag",
+        "Fredag",
+        "Lørdag"
+    ]
+    
+    return days[date];
+}
+
+function getMonthName(monthIndex) {
+    
+    const months = [
+        "januar",
+        "februar",
+        "marts",
+        "april",
+        "maj",
+        "juni",
+        "juli",
+        "august",
+        "september",
+        "oktober",
+        "november",
+        "december"
+    ]
+
+    return months[monthIndex];
 }
