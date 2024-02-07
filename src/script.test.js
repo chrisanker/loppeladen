2a42a8f282d363cb9f185a0b542432468c96b588
Christian Anker Larsen <calanker@gmail.com>
2024-02-07 23:31:53 +0100

    adding failing test

--- src/script.test.js
+++ src/script.test.js
@@ -4,3 +4,8 @@ it('returns formated month', () => {
     const result = getMonthName(1);
     expect(result).toBe('februar');
 });
+
+it('returns formated month', () => {
+    const result = getMonthName(0);
+    expect(result).toBe('februar');
+});
